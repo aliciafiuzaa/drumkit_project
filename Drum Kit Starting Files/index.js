43df576938dda72c44b0db2d63bb5ac61cf2ab2a
Alícia Fiúza <56484179+aliciafiuzaa@users.noreply.github.com>
2022-06-26 21:39:13 +0100

    Drum kit project

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,56 @@
+
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numberOfDrumButtons; i++) {
+  document.getElementsByTagName("button")[i].addEventListener("click", function() {
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+const makeSound = (keyPressed) => {
+  switch (keyPressed) {
+    case "w":
+      const crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "a":
+      const kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    case "s":
+      const snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "d":
+      const tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    default: console.log(buttonInnerHTML);
+  }
+}
+
+const buttonAnimation = (currentKey) => {
+  let activeButton = document.querySelector("." + currentKey)
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
